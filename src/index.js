5697b57f46e991adde88f5ddae0230207f1e4ffa
Apurva729 <apurva.22010877@viit.ac.in>
2023-12-07 16:45:32 +0530

    Complete Files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export {Header} from "./components/Header";
+export {Footer} from "./components/Footer";
+export {Card} from "./components/Card";
+export {Button} from "./components/Button";
+export {ScrollToTop} from "./components/ScrollToTop";
